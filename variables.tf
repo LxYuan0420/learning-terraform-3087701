f591a59ac9691bad8dd0fcef9af02698eaf1b0b3
Yuan Lik Xun <lxyuan0420@gmail.com>
2025-03-22 12:13:38 +0800

    Parameterize EC2 Instance Type and AWS Region
    - Exercise: Edit your code
    
    - Updated `main.tf` to use a variable for the `instance_type` of the
      `aws_instance` resource, allowing for customizable instance types.
    
    - Uncommented and adjusted `outputs.tf` to expose the instance AMI and
      ARN publicly.
    
    - Modified `providers.tf` to parameterize the AWS region using a
      variable, enhancing flexibility in deployment locations.
    
    - Defined new variable `aws_region` with a default value of
      `ap-southeast-1` in `variables.tf`.
    
    - Ensured `instance_type` variable is active and retains a default of
      `t3.nano` for ease of configuration.

--- variables.tf
+++ variables.tf
@@ -1,4 +1,9 @@
-#variable "instance_type" {
-#  description = "Type of EC2 instance to provision"
-#  default     = "t3.nano"
-#}
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t3.nano"
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers"
+  default     = "ap-southeast-1"
+}
